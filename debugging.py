92b921d29c1c454d6eb06f0d02bed56d4d2cb2eb
Wilfredo <wifredo.gonzalez2019@gmail.com>
2026-02-13 20:07:27 -0600

    Debugging arreglado

--- debugging.py
+++ debugging.py
@@ -1,17 +1,11 @@
 password = input("Clave: ")
 
-# Agregar prints para investigar
-
 print(f"DEBUG tipo: {type(password)}")
-
 print(f"DEBUG valor: [{password}]")
-
 print(f"DEBUG len: {len(password)}")
 
-if password == 1234: # Bug aquí!
-
-print("Acceso concedido")
-
+if password == "1234":
+    print("Acceso concedido")
 else:
+    print("Acceso denegado")
 
-print("Acceso denegado")
